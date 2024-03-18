73a9379d6e33e73122678416974162a4363eab13
Loboyko Ilya <loboykois@gmail.com>
2024-03-19 00:00:07 +0200

    test(DBA): add new test for navigation panel

--- pageObjects/header.ts
+++ pageObjects/header.ts
@@ -23,4 +23,28 @@ export class Header {
   public async navigationMenu(navItem: string): Promise<void> {
     await this.navbar.getByText(navItem).click();
   }
+
+  public async showContactsPopup(): Promise<void> {
+    await this.navigationMenu(NavigationBar.Contact);
+    const contactsPopup = this.page.locator(".modal-dialog").first();
+    await contactsPopup.waitFor({ state: "visible" });
+  }
+
+  public async showAboutUsPopup(): Promise<void> {
+    await this.navigationMenu(NavigationBar.AboutUs);
+    const aboutUsPopup = this.page.locator("#videoModal");
+    await aboutUsPopup.waitFor({ state: "visible" });
+  }
+
+  public async showLoginPopup(): Promise<void> {
+    await this.navigationMenu(NavigationBar.LogIn);
+    const loginPopup = this.page.locator("#logInModal");
+    await loginPopup.waitFor({ state: "visible" });
+  }
+
+  public async showSignUpPopup(): Promise<void> {
+    await this.navigationMenu(NavigationBar.SignUp);
+    const signInPopup = this.page.locator("#signInModal");
+    await signInPopup.waitFor({ state: "visible" });
+  }
 }
--- tests/demoBlazeTests.spec.ts
+++ tests/demoBlazeTests.spec.ts
@@ -8,15 +8,57 @@ test.describe("Demo Blaze tests", () => {
     await homePage.navigate();
   });
 
-  test("should return on home page when clicking on logo", async ({ page, homePage }) => {
-    await homePage.header.clickOnLogo();
+  test.describe("should redirect to", () => {
+    test("should redirect to home page when clicking on logo", async ({ page, homePage }) => {
+      await homePage.header.clickOnLogo();
 
-    await expect(page).toHaveURL(Routes.HomePage);
+      await expect(page).toHaveURL(Routes.HomePage);
+    });
+
+    test("should redirect to home page when clicking on Home page in navigation bar", async ({ page, homePage }) => {
+      await homePage.header.navigationMenu(NavigationBar.Home);
+
+      await expect(page).toHaveURL(Routes.HomePage);
+    });
+
+    test("should redirect to cart page when clicking on Cart in navigation bar", async ({ page, homePage }) => {
+      await homePage.header.navigationMenu(NavigationBar.Cart);
+
+      await expect(page).toHaveURL(Routes.CartPage);
+    });
   });
 
-  test("should return on home page when clicking on Home page in navigation bar", async ({ page, homePage }) => {
-    await homePage.header.navigationMenu(NavigationBar.Home);
+  test.describe("should open modal window", () => {
+    test("New message when user clicks on Contacts", async ({ page, homePage }) => {
+      await homePage.header.showContactsPopup();
 
-    await expect(page).toHaveURL(Routes.HomePage);
+      const modalWindow = page.locator(".modal-dialog").first();
+
+      expect(modalWindow).toBeTruthy();
+    });
+
+    test("About us when user clicks on About us in navigation", async ({ page, homePage }) => {
+      await homePage.header.showAboutUsPopup();
+
+      const modalWindow = page.locator("#videoModal");
+
+      expect(modalWindow).toBeTruthy();
+    });
+
+    test("LogIn when user clicks on LogIn in navigation", async ({ page, homePage }) => {
+      await homePage.header.showLoginPopup();
+
+      const modalWindow = page.locator("#logInModal");
+
+      expect(modalWindow).toBeTruthy();
+    });
+
+    test("Sign up when user clicks on Sign up in navigation", async ({ page, homePage }) => {
+      await homePage.header.showSignUpPopup();
+
+      const modalWindow = page.locator("#signInModal");
+
+      expect(modalWindow).toBeTruthy();
+    });
   });
 });
